00a565342292a6b41508d3d34055bf8af4e1fc81
Filipe Macedo <devmacedo@gmail.com>
2019-11-23 20:58:10 -0300

    :sparkles: timeout to disable animation

--- src/components/CardMsg/index.js
+++ src/components/CardMsg/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useToggleModal } from "../../hooks/toggle-modal.hook";
 import { Store } from "../../store/config/config";
 import { setTweetInformations } from "../../store/modules/modal/actions";
@@ -10,14 +10,18 @@ import {
   UserMsg,
   Photo
 } from "./card-msg.styles";
+import { useTimeout } from "../../hooks/timeout.hook";
 
 const CardMsg = ({
   userImage,
   userScreenName,
+  index,
   text,
   className,
   openModalOnClick = true
 }) => {
+  const isFinishedTime = useTimeout(200 * index);
+
   const [, toggleCardModal] = useToggleModal();
   const [, dispatch] = useContext(Store);
 
@@ -25,20 +29,22 @@ const CardMsg = ({
     dispatch(setTweetInformations({ userImage, userScreenName, text }));
 
   return (
-    <Container className={className}>
-      <Card
-        onClick={() => {
-          toggleCardModal(openModalOnClick);
-          dispatchTweetInformations();
-        }}
-      >
-        <UserPhoto>
-          <Photo src={userImage} />
-        </UserPhoto>
-        <UserName>{userScreenName}</UserName>
-        <UserMsg>{text}</UserMsg>
-      </Card>
-    </Container>
+    isFinishedTime && (
+      <Container className={className}>
+        <Card
+          onClick={() => {
+            toggleCardModal(openModalOnClick);
+            dispatchTweetInformations();
+          }}
+        >
+          <UserPhoto>
+            <Photo src={userImage} />
+          </UserPhoto>
+          <UserName>{userScreenName}</UserName>
+          <UserMsg>{text}</UserMsg>
+        </Card>
+      </Container>
+    )
   );
 };
 
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Container, Wrapper, CardGrid } from "./main.styles";
 import CardMsg from "../../components/CardMsg";
@@ -6,25 +6,34 @@ import Loading from "../../components/Loading";
 import { useTweets } from "../../hooks/tweets.hook";
 import { useOnNewTweet } from "../../hooks/on-new-tweet.hook";
 import CardModal from "../../components/CardModal";
+import { useTimeout } from "../../hooks/timeout.hook";
 
 export default function App() {
   const [tweets, setTweets] = useTweets();
+  const isDisabledLoadind = useTimeout(1800);
 
   const addNewTweet = tweet => setTweets([tweet, ...tweets]);
 
   useOnNewTweet({ addNewTweet, tweets });
 
+  const hasTweets = tweets.length;
+
   return (
     <Container>
-      <CardModal />
-      <Loading />
-      <Wrapper>
-        <CardGrid>
-          {tweets.map(props => (
-            <CardMsg {...props} />
-          ))}
-        </CardGrid>
-      </Wrapper>
+      {!hasTweets || !isDisabledLoadind ? (
+        <Loading />
+      ) : (
+        <>
+          <CardModal />
+          <Wrapper>
+            <CardGrid>
+              {tweets.map((props, index) => (
+                <CardMsg {...props} index={index} />
+              ))}
+            </CardGrid>
+          </Wrapper>
+        </>
+      )}
     </Container>
   );
 }
